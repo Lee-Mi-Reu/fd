a8bf9ecf02a8bee0e1a15bfa610e44c232ffe4f2
Lee-Mi-Reu <36959154+Lee-Mi-Reu@users.noreply.github.com>
2018-03-01 23:41:38 +0900

    Update jage.js

--- jage.js
+++ jage.js
@@ -1,3 +1,4 @@
 jQuery("#wcms > form > div > table > tbody > tr.first > td._writer_area.writer").each(function (i, e) {
     console.log(e.text);
 });
+console.log(jQuery("#wcms > form > div > table > tbody > tr.first > td._writer_area.writer").length);
