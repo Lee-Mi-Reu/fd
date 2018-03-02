deaccca22f4482c11a7b0158e7c8ee831b0bf81e
Lee-Mi-Reu <36959154+Lee-Mi-Reu@users.noreply.github.com>
2018-03-03 01:26:57 +0900

    Add files via upload

--- jage.js
+++ jage.js
@@ -1,10 +1,17 @@
-var chadan = ['hyunmin', 'br']
 jQuery("#wcms > form > div > table > tbody > tr > td._writer_area.writer").each(function (i, e) {
     e = jQuery(e);
+    var chadan;
+    jQuery.ajax({
+        url: "https://rawgit.com/Lee-Mi-Reu/fd/master/chadan.json",
+        dataType: "json",
+        success: function (data) {
+            chadan = data;
+            console.log(data);
+        }
+    })
     chadan.forEach(function (v) {
         if (!e.text().indexOf(v)) {
             e.parentsUntil('.tr')[0].remove();
         }
-    })
+    });
 });
-console.log(jQuery("#wcms > form > div > table > tbody > tr > td._writer_area.writer").length);
