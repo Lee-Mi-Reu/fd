192e614d56fc8defd61d1ddde67abcbd7b1388e1
Lee-Mi-Reu <36959154+Lee-Mi-Reu@users.noreply.github.com>
2018-03-03 01:46:08 +0900

    Update jage.js

--- jage.js
+++ jage.js
@@ -3,8 +3,8 @@ jQuery("#wcms > form > div > table > tbody > tr > td._writer_area.writer").each(
     var chadan;
     jQuery.ajax({
         url: "https://rawgit.com/Lee-Mi-Reu/fd/master/chadan.json",
+        dataType: "JSON",
         success: function (data) {
-            data = JSON.parse(data);
             data.forEach(function (v) {
                 if (!e.text().indexOf(v)) {
                     e.parentsUntil('.tr')[0].remove();
