3117a0c4480900c341007bc3c3dc41400e9f04e4
Lee-Mi-Reu <36959154+Lee-Mi-Reu@users.noreply.github.com>
2018-03-07 23:17:04 +0900

    Update jage.js

--- jage.js
+++ jage.js
@@ -1,15 +1,14 @@
-jQuery("#wcms > form > div > table > tbody > tr > td._writer_area.writer").each(function (i, e) {
-    e = jQuery(e);
-    var chadan;
-    jQuery.ajax({
-        url: "https://rawgit.com/Lee-Mi-Reu/fd/master/chadan.json",
-        dataType: "JSON",
-        success: function (data) {
-            data.forEach(function (v) {
+jQuery.ajax({
+    url: "https://rawgit.com/Lee-Mi-Reu/fd/master/chadan.json",
+    success: function (data) {
+        data = JSON.parse(data);
+        data.forEach(function (v) {
+            jQuery("#wcms > form > div > table > tbody > tr > td._writer_area.writer").each(function (i, e) {
+                e = jQuery(e);
                 if (!e.text().indexOf(v)) {
                     e.parentsUntil('.tr')[0].remove();
                 }
             });
-        }
-    })
+        });
+    }
 });
