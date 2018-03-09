a2fe851211d587d71916ad8eb4a3e609f24e2b01
Lee-Mi-Reu <36959154+Lee-Mi-Reu@users.noreply.github.com>
2018-03-09 13:26:25 +0900

    Update newcastle.js

--- newcastle.js
+++ newcastle.js
@@ -1,16 +1,51 @@
 function cha(obj) {
-    var player = jQuery("img[src*='"+obj.ori+"_73']");
-    player.prop("src", obj.imgMin);
-    var grandFather = player.parent().parent();
-    grandFather.siblings(".name_area").find(".name").text(obj.name);
-    if (obj.imgBig) {
-        grandFather.siblings(".plr_btn").on("click", ".plr_more", function () {
-            setTimeout(function () {
-                var nation_name = jQuery(".plr_name_big").html();
-                jQuery(".plr_name_big").html(nation_name.substring(0, nation_name.lastIndexOf('>')+1)+obj.name);
-                jQuery(".plr_pic > img").prop("src", obj.imgBig);
-            }, 1000);
-        });
+    if (obj.name) {
+        var player = jQuery("img[src*='"+obj.ori+"_73']");
+        player.prop("src", obj.imgMin);
+        var grandFather = player.parent().parent();
+        grandFather.siblings(".name_area").find(".name").text(obj.name);
+        if (obj.imgBig) {
+            grandFather.siblings(".plr_btn").on("click", ".plr_more", function () {
+                setTimeout(function () {
+                    var nation_name = jQuery(".plr_name_big").html();
+                    jQuery(".plr_name_big").html(nation_name.substring(0, nation_name.lastIndexOf('>')+1)+obj.name);
+                    jQuery(".plr_pic > img").prop("src", obj.imgBig)
+                }, 1000)
+            })
+        }
+    }
+    if (obj.rating) {
+        var bigCard = jQuery(".ex_card > .crd_sec");
+        var ratingClassArr = bigCard.prop("class").split(" ");
+        var ratingClass = "";
+        for (var i = 0; i < 2; i++) {
+            ratingClass += ratingClassArr[i]+" "
+        }
+        ratingClass += obj.rating+" ";
+        switch (obj.rating) {
+            case "ace" :
+            case "l" :
+            case "e" :
+            case "w" :
+            ratingClass += "max10_"+obj.rating;
+            break;
+            case "nlive" :
+            ratingClass += obj.rating;
+            break;
+            case "s" :
+            ratingClass += "max9_"+obj.rating;
+            break;
+            case "n" :
+            ratingClass += "max8_"+obj.rating;
+            break
+        }
+        bigCard.prop("class", ratingClass)
+    }
+    if (obj.level) {
+        jQuery(".plr_level").text(obj.level)
+    }
+    if (obj.poten) {
+        jQuery('.crd_box > span.up').addClass("num"+obj.poten);
     }
 }
 function pp() {
@@ -19,17 +54,17 @@ function pp() {
     });
     var rafa = '<div class="card shadow ace  ui-draggable ui-droppable cvr_up2" style="left: 88px; top: 269px; display: block; transition-property: left, top; transition-duration: 0.25s; transition-timing-function: ease-out;">            <div class="crd_sec lg1 ace">                <div class="photo">                    <span class="player" style="background:url(http://images.fd.toastoven.net/pc/img_g/data/club_license/card_110_73_3.png);"><img src="https://i.imgur.com/hEJxcf4.png" alt="" width="73" height="57"></span><span style="background-image: url(https://i.imgur.com/CQoe5he.png);" class="position"></span>                   <span class="level lv10_ace"><span class="blind">level</span></span>                                        <span class="flag "><img src="http://images.fd.toastoven.net/pc/img_g/data/flag/002_20.png" width="20" height="13" alt=""></span>                    <span class="item_t" data-injurycnt="0"><span class="blind">컨디션</span></span>                    <span class="up num10">potential</span>                    <div class="_poten_img" style="width : 73px;height:57px; overflow: hidden;position: absolute; z-index: 2"><div style="left: 0px; top: 0px; width: 73px; height: 57px; background: url(&quot;http://images.fd.toastoven.net/pc/img_g/ani_small.png&quot;) 0px center; opacity: 1;"></div></div><div class="bg_rainbow _rainbowEffectDiv"><img class="move_rainbow" style="animation-iteration-count: infinite; animation-timing-function: linear; animation-name: rotate0; animation-duration: 2s;"></div>                                                                            </div>                <div class="name_area">                    <div class="name">라파 베니테즈</div>                    <div class="point"><span style="width:100%;"></span></div>                </div>                                <span class="plr_btn" style="display: none;"><a href="#" class="plr_more">더보기</a></span>            </div>		</div>';
     jQuery("dl.dl_lninfo > dd.plr").append(rafa);
-    var update_2;
+    var update_2
 }
 setTimeout(function () {
     pp();
     jQuery(".naver_sports")
     .after(jQuery("<img>").prop({width: 30, src: "https://i.imgur.com/OwpYc3T.png"}).addClass("mir-god"))
-    .remove();
+    .remove()
 }, 1000);
 jQuery("#gnb").on("click", ".mir-god", pp);
 jQuery("a[data-target$='lineup']").on("click", function () {
     setTimeout(function () {
-        pp();
-    }, 1000);
+        pp()
+    }, 1000)
 });
